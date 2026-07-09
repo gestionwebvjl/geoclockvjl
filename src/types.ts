dd6b133c47a40bc904c29fb81b5746101ae50238
gestionwebvjl <gestionwebvjl@gmail.com>
2026-07-09 21:03:38 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -55,7 +55,7 @@ export function useGeolocation() {
         setError(null);
       },
       (err) => { setError(err.message); },
-      { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 }
+      { enableHighAccuracy: true, timeout: 30000, maximumAge: 0 }
     );
     return () => navigator.geolocation.clearWatch(watchId);
   }, []);
